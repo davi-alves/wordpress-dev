82676e5328a89f374147cb1c499eccf68ea6b30f
Davi Alves <flyingkrai@hotmail.com>
2013-06-29 22:27:33 -0300

    project

---
+++ wp-content/plugins/flyingkrai_featured_slider/app-class.php
@@ -0,0 +1,7 @@
+<?php
+
+// create plugin class
+// add static registration methods
+// add static class instance method
+// add plugin initialization in cosntructor
+// expose public methods
---
+++ wp-content/plugins/flyingkrai_featured_slider/app.php
@@ -0,0 +1,8 @@
+<?php
+
+// write plugin description
+// check direct access
+// check if theme suports add_image_size using current_theme_supports()
+// call plugin class
+// register activation hooks
+// initialize class
